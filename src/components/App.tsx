74e1fd9a6aa209474fe52007fdbcce249b4c0665
Alex Burner <alex.burner@gmail.com>
2018-05-18 12:37:54 -0700

    Restore subtitle

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,10 @@ interface Props {
 
 const App = ({ site }: Props): JSX.Element => (
   <div className="app">
-    <h1>{site.title}</h1>
+    <h1>
+      {site.title}
+      <span className="subtitle">{site.subtitle}</span>
+    </h1>
     {site.sections.map(section => (
       <Section key={section.title} section={section} />
     ))}
